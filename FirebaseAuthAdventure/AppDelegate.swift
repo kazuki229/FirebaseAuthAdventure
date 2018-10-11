2694358223c484efcbc3bd53a694a65ff8676930
kazuki229 <tsuzuku.k@gmail.com>
2018-10-11 21:49:08 +0900

    :tada: initial commit

---
+++ FirebaseAuthAdventure/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  FirebaseAuthAdventure
+//
+//  Created by 都筑一希 on 2018/10/11.
+//  Copyright © 2018 kazuki229. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+        ) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+}
---
+++ FirebaseAuthAdventure/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FirebaseAuthAdventure
+//
+//  Created by 都筑一希 on 2018/10/11.
+//  Copyright © 2018 kazuki229. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
